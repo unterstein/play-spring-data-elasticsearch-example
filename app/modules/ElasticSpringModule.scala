e88aff710c45e46a11d05c644c1253785aed31de
Johannes Unterstein <unterstein@me.com>
2016-02-19 10:30:59 +0100

    cleanup

--- app/modules/ElasticSpringModule.scala
+++ app/modules/ElasticSpringModule.scala
@@ -8,11 +8,4 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.guice.module.SpringModule
 
 
-class ElasticSpringModule extends SpringModule(new AnnotationConfigApplicationContext(classOf[ElasticSearchConfiguration])) {
-
-}
-//class ElasticSpringModule extends SpringModule {
-//  public MySpringModule() {
-//    super(new AnnotationConfigApplicationContext(ElasticSearchConfiguration.class))
-//  }
-//}
+class ElasticSpringModule extends SpringModule(new AnnotationConfigApplicationContext(classOf[ElasticSearchConfiguration]))
